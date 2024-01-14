405fa8938c6eecd2e06e05ca7e368eb9f0a603dc
C4T4LYST <C4T4LYST@DESKTOP-4FT7KNC>
2024-01-14 12:37:22 +0100

    .

--- src/handler.js
+++ src/handler.js
@@ -13,8 +13,55 @@ function hideQuickMenu() {
 }
 
 function updateSearch(input) {
+    input = input.toLowerCase();
     //srach for 1/1 match inputs
-    
+    let resultFirst = searchBarOptions.filter(option => option.displayName === input);
+    //search for partial match inputs
+    let resultSecond = searchBarOptions.filter(option => option.displayName.includes(input));
+    //Search for most matched letters
+    let resultsThird = searchBarOptions.map(option => {
+        let matchesCount = 0;
+        for (let i = 0; i < input.length; i++) {
+            if(option.displayName.length < input.length) {
+                break;
+            }
+
+            if (option.displayName.split('').includes(input[i])) {
+                matchesCount++;
+            }
+        }
+
+        return {displayName: option.displayName, searchFunction: option.searchFunction, matchesCount: matchesCount};
+    }).sort((a, b) => b.matchesCount - a.matchesCount).filter(option => option.matchesCount > 0);
+
+    let results = [...resultFirst, ...resultSecond, ...resultsThird];
+
+    let inListAlready = [];
+    results = results.filter(result => {
+        if (inListAlready.includes(result.displayName)) {
+            return false;
+        }
+        else {
+            inListAlready.push(result.displayName);
+            return true;
+        }
+    });
+
+    quickMenuSearchResults.innerHTML = '';
+    searchResults = [];
+
+    results.forEach(result => {
+        let resultElement = document.createElement('p');
+        resultElement.classList.add('searchResult');
+        resultElement.innerText = result.displayName;
+        resultElement.addEventListener('click', function() {
+            result.searchFunction();
+            hideQuickMenu();
+        });
+
+        quickMenuSearchResults.appendChild(resultElement);
+        searchResults.push(resultElement);
+    });
 }
 
 quickMenuSearchBar.addEventListener('click', function(event) {
@@ -28,7 +75,26 @@ quickMenuSearchBar.addEventListener('keydown', function(event) {
         hideQuickMenu();
     }
     else if (event.key === 'Enter') {
-
+        searchResults[searchResultsSelectedIndex].click();
+        hideQuickMenu();
+    }
+    else if (event.key === 'ArrowUp') {
+        if (searchResultsSelectedIndex > 0) {
+            searchResultsSelectedIndex--;
+            searchResults[searchResultsSelectedIndex].classList.add('selected');
+            searchResults[searchResultsSelectedIndex + 1].classList.remove('selected');
+        }
+    }
+    else if (event.key === 'ArrowDown') {
+        if (searchResultsSelectedIndex < searchResults.length - 1) {
+            searchResultsSelectedIndex++;
+            searchResults[searchResultsSelectedIndex].classList.add('selected');
+            searchResults[searchResultsSelectedIndex - 1].classList.remove('selected');
+        }
+    }
+    else {
+        updateSearch(quickMenuSearchBar.value);
+    }
 });
 
 quickMenuSearchResults.addEventListener('click', function(event) {
--- src/searchBar.js
+++ src/searchBar.js
@@ -1,9 +1,29 @@
 
 let searchResults = [];
 let searchBarOptions = [];
+let searchResultsSelectedIndex = 0;
 
 function addSearchOption(displayName, searchFunction) {
-    searchBarOptions.push({displayName: displayName, searchFunction: searchFunction});
+    searchBarOptions.push({displayName: displayName.toLowerCase(), searchFunction: searchFunction});
 }
 
 addSearchOption('Create start point', () => {});
+addSearchOption('getElementsByName', () => {});
+addSearchOption('getElementsByClassName', () => {});
+addSearchOption('getElementsByTagName', () => {});
+addSearchOption('querySelector', () => {});
+addSearchOption('querySelectorAll', () => {});
+addSearchOption('getElementById', () => {});
+
+addSearchOption('getWindowName', () => {});
+addSearchOption('getWindowLocation', () => {});
+addSearchOption('getWindowHistory', () => {});
+
+addSearchOption('While loop', () => {});
+addSearchOption('For loop', () => {});
+addSearchOption('For in loop', () => {});
+addSearchOption('For of loop', () => {});
+addSearchOption('Do while loop', () => {});
+addSearchOption('Switch statement', () => {});
+addSearchOption('If statement', () => {});
+
