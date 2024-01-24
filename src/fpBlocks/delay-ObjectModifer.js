9143f67b965cac9976110f3118c96c5a9c59b100
C4T4LYST <C4T4LYST@DESKTOP-4FT7KNC>
2024-01-25 00:09:45 +0100

    .

---
+++ src/fpBlocks/delay-ObjectModifer.js
@@ -0,0 +1,21 @@
+class ObjectDeserializer extends ioblockTimeLine {
+    constructor() {
+        super();
+    }
+    getValue() {
+        return super.getValue('obj')[super.getValue('field')];
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.setSize(120, 120);
+        this.classList.add('object-deserializer');
+
+        this.addInput('obj', 'any');
+        this.addInput('field', 'string');
+        this.addInput('data', 'any');
+        this.addOutput('obj', 'any');
+    }
+}
+
+customElements.define('object-simple-modifyer', ObjectDeserializer);
